5b901ce00d580f10865053a68e8cbc935935513c
adam <athornton@gmail.com>
2017-05-30 15:16:23 -0700

    Fix substitution.

--- docker/substitute_config.sh
+++ docker/substitute_config.sh
@@ -11,6 +11,15 @@ set_config() {
     sed -i "s/{{${KEY}}}/${VALUE}/g" /etc/rabbitmq/rabbitmq.config
 }
 
+if [ -z "${RABBITMQ_PAN_PASSWORD}" ] || [ -z "${RABBITMQ_TARGET_HOST}" ] || \
+       [ -z "${RABBITMQ_TARGET_QUEUE}" ]; then
+    echo 1>&2 "All of RABBITMQ_PAN_PASSWORD, RABBITMQ_TARGET_HOST, and RABBITMQ_TARGET_QUEUE"
+    echo 1>&2 " must be set!"
+    # Don't respawn too fast
+    sleep 60
+    exit 2
+fi
+
 set_config RABBITMQ_PAN_PASSWORD ${RABBITMQ_PAN_PASSWORD}
 set_config RABBITMQ_TARGET_HOST ${RABBITMQ_TARGET_HOST}
 set_config RABBITMQ_TARGET_QUEUE ${RABBITMQ_TARGET_QUEUE}
