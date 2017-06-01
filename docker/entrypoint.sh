2d532eb3aa525492bc27c91a669fa9ae5fa07a48
J. Matt Peterson <jmatt@jmatt.org>
2017-06-01 13:47:39 -0700

    Merge dynamic shovel changes and ajt branch changes.
    	modified:   docker/Dockerfile
    	modified:   docker/entrypoint.sh
    	modified:   docker/logstash.ini
    	modified:   docker/rabbitmq.config
    	modified:   docker/rabbitmq.ini
    	new file:   docker/rabbitmq.shovel.definitions.json
    	modified:   docker/substitute_config.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,29 +1,11 @@
 #!/bin/sh
 set -e
 
-set_config() {
-    KEY=$1
-    VALUE=$2
-    if [ -z "${VALUE}" ]; then
-	echo "${KEY} must be set" 1>&2
-	exit 1
-    fi
-    sed -i "s/{{${KEY}}}/${VALUE}/g" ${RABBITMQ_CONFIG}/rabbitmq.config
-}
-
-if [ -z "${RABBITMQ_PAN_PASSWORD}" ]; then
-    echo "RABBITMQ_PAN_PASSWORD must be set." 1>&2
-    exit 1
+/substitute_config.sh
+rc=$?
+if [ ${rc} -ne 0 ]; then
+    echo 1>&2 "Environment substitution failed.  Exiting in 60 seconds."
+    sleep 60
+    exit ${rc}
 fi
-
-if [ -z "${RABBITMQ_TARGET_HOST}" ]; then
-    echo "RABBITMQ_TARGET_HOST must be set." 1>&2
-    exit 1
-fi
-
-if [ -z "${RABBITMQ_TARGET_VHOST}" ]; then
-    echo "RABBITMQ_TARGET_VHOST must be set." 1>&2
-    exit 1
-fi
-
-/usr/bin/supervisord -c /etc/supervisord.conf
+exec /usr/bin/supervisord -c /etc/supervisord.conf -n
--- docker/substitute_config.sh
+++ docker/substitute_config.sh
@@ -4,22 +4,27 @@ set -e
 set_config() {
     KEY=$1
     VALUE=$2
+    FILE=$3
     if [ -z "${VALUE}" ]; then
 	echo "${KEY} must be set" 1>&2
 	exit 1
     fi
-    sed -i "s/{{${KEY}}}/${VALUE}/g" /etc/rabbitmq/rabbitmq.config
+    sed -i "s/{{${KEY}}}/${VALUE}/g" ${FILE}
 }
 
 if [ -z "${RABBITMQ_PAN_PASSWORD}" ] || [ -z "${RABBITMQ_TARGET_HOST}" ] || \
-       [ -z "${RABBITMQ_TARGET_QUEUE}" ]; then
-    echo 1>&2 "All of RABBITMQ_PAN_PASSWORD, RABBITMQ_TARGET_HOST, and RABBITMQ_TARGET_QUEUE"
+       [ -z "${RABBITMQ_TARGET_VHOST}" ]; then
+    echo 1>&2 "All of RABBITMQ_PAN_PASSWORD, RABBITMQ_TARGET_HOST, and RABBITMQ_TARGET_VHOST"
     echo 1>&2 " must be set!"
     # Don't respawn too fast
-    sleep 60
     exit 2
 fi
 
-set_config RABBITMQ_PAN_PASSWORD ${RABBITMQ_PAN_PASSWORD}
-set_config RABBITMQ_TARGET_HOST ${RABBITMQ_TARGET_HOST}
-set_config RABBITMQ_TARGET_QUEUE ${RABBITMQ_TARGET_QUEUE}
+RMQ="/etc/rabbitmq/rabbitmq.config"
+SUP="/etc/supervisord.conf"
+set_config RABBITMQ_PAN_PASSWORD ${RABBITMQ_PAN_PASSWORD} ${RMQ}
+set_config RABBITMQ_TARGET_HOST ${RABBITMQ_TARGET_HOST} ${RMQ}
+set_config RABBITMQ_TARGET_QUEUE ${RABBITMQ_TARGET_VHOST} ${RMQ}
+SUPERVISORD_PASSWORD=$(dd if=/dev/urandom bs=1 count=16 2>/dev/null | \
+			   base64 -i - | sed -e 's|/|_|g')
+set_config SUPERVISORD_PASSWORD ${SUPERVISORD_PASSWORD} ${SUP}
