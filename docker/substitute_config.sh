f3e03961bd3d881d6553fc1f300b862f2b0b92c9
J. Matt Peterson <jmatt@jmatt.org>
2017-06-01 13:40:13 -0700

    Use dynamic shovel instead of static shovel.
    	modified:   docker/Dockerfile
    	modified:   docker/rabbitmq.config
    	new file:   docker/rabbitmq.shovel.definitions.json
    	modified:   docker/substitute_config.sh

--- docker/substitute_config.sh
+++ docker/substitute_config.sh
@@ -13,8 +13,8 @@ set_config() {
 }
 
 if [ -z "${RABBITMQ_PAN_PASSWORD}" ] || [ -z "${RABBITMQ_TARGET_HOST}" ] || \
-       [ -z "${RABBITMQ_TARGET_QUEUE}" ]; then
-    echo 1>&2 "All of RABBITMQ_PAN_PASSWORD, RABBITMQ_TARGET_HOST, and RABBITMQ_TARGET_QUEUE"
+       [ -z "${RABBITMQ_TARGET_VHOST}" ]; then
+    echo 1>&2 "All of RABBITMQ_PAN_PASSWORD, RABBITMQ_TARGET_HOST, and RABBITMQ_TARGET_VHOST"
     echo 1>&2 " must be set!"
     # Don't respawn too fast
     exit 2
@@ -24,7 +24,7 @@ RMQ="/etc/rabbitmq/rabbitmq.config"
 SUP="/etc/supervisord.conf"
 set_config RABBITMQ_PAN_PASSWORD ${RABBITMQ_PAN_PASSWORD} ${RMQ}
 set_config RABBITMQ_TARGET_HOST ${RABBITMQ_TARGET_HOST} ${RMQ}
-set_config RABBITMQ_TARGET_QUEUE ${RABBITMQ_TARGET_QUEUE} ${RMQ}
+set_config RABBITMQ_TARGET_QUEUE ${RABBITMQ_TARGET_VHOST} ${RMQ}
 SUPERVISORD_PASSWORD=$(dd if=/dev/urandom bs=1 count=16 2>/dev/null | \
 			   base64 -i - | sed -e 's|/|_|g')
 set_config SUPERVISORD_PASSWORD ${SUPERVISORD_PASSWORD} ${SUP}
